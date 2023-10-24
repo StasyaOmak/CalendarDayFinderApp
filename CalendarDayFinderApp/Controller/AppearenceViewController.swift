031d3713a5b1fd3cb2f9509ba481996c2afc9f7f
Anastasiya <ios.developer.by@gmail.com>
2023-10-24 11:59:24 +0300

    Logic for settings

--- CalendarDayFinderApp/Controller/AppearenceViewController.swift
+++ CalendarDayFinderApp/Controller/AppearenceViewController.swift
@@ -8,28 +8,34 @@
 import UIKit
 
 class AppearenceViewController: UIViewController {
-
+    
     @IBOutlet weak var textLabel: UILabel!
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var settingButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         setLabelText()
     }
     
+    
     @IBAction func closeItemTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+#warning("Open iOS simulator setting app, UIApplication")
     
     @IBAction func openSettings(_ sender: Any) {
-        #warning("Open iOS simulator setting app, UIApplication")
+        UIApplication.shared.open(URL(string: "App-prefs:Bluetooth")!)
     }
     
+#warning("Light Mode is On to Dark Mode is On")
+    
     func setLabelText() {
-        var labelText = "Unable specify UI style"
-        textLabel.text = labelText
-        #warning("Light Mode is On to Dark Mode is On")
+        textLabel.text = self.traitCollection.userInterfaceStyle == .dark ? "Dark mode is on" : "Light mode is on"
+        }
     }
-}
+
 
 extension AppearenceViewController {
     override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
